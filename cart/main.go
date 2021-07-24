a256a1364e87facee943c59b3f22d9c33c37d705
adplt <atriadplt@gmail.com>
2021-07-24 19:20:56 +0700

    add example for input in console app

--- cart/main.go
+++ cart/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	configs "project/app/configs"
 )
 
 func main() {
 	configs.Block{
 		Try: func() {
-
+			var (
+				input string
+			)
+			scanner := bufio.NewScanner(os.Stdin)
+			if scanner.Scan() {
+				input = scanner.Text()
+				fmt.Printf("Input was: %s\n", input)
+			}
 		},
 		Catch: func(e error) {
 			fmt.Println(e)
