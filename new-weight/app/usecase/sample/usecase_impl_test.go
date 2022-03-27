e5b5e18e4cdceeb5667ddfb750f57c9c104f2ebf
adplt <atriadplt@gmail.com>
2022-03-27 23:01:43 +0700

    finishing test case

--- new-weight/app/usecase/sample/usecase_impl_test.go
+++ new-weight/app/usecase/sample/usecase_impl_test.go
@@ -6,9 +6,10 @@ import (
 	"project-name/app/usecase/sample/response"
 	"reflect"
 	"testing"
+	"time"
 )
 
-func Test_sampleUseCase_GetWeight(t *testing.T) {
+func Test_sampleUseCase_GetWeight_1(t *testing.T) {
 	type fields struct {
 		restAPI rest_api.RestAPI
 	}
@@ -31,19 +32,107 @@ func Test_sampleUseCase_GetWeight(t *testing.T) {
 			uc := &sampleUseCase{
 				restAPI: tt.fields.restAPI,
 			}
+			tt.args.in.Limit = 10
+			tt.args.in.Offset = 0
 			gotOut, gotHttpCode, gotCode, err := uc.GetWeight(tt.args.in)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("GetWeight() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetWeight(1) error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(gotOut, tt.wantOut) {
-				t.Errorf("GetWeight() gotOut = %v, want %v", gotOut, tt.wantOut)
+				t.Errorf("GetWeight(1) gotOut = %v, want %v", gotOut, tt.wantOut)
 			}
 			if gotHttpCode != tt.wantHttpCode {
-				t.Errorf("GetWeight() gotHttpCode = %v, want %v", gotHttpCode, tt.wantHttpCode)
+				t.Errorf("GetWeight(1) gotHttpCode = %v, want %v", gotHttpCode, tt.wantHttpCode)
 			}
 			if gotCode != tt.wantCode {
-				t.Errorf("GetWeight() gotCode = %v, want %v", gotCode, tt.wantCode)
+				t.Errorf("GetWeight(1) gotCode = %v, want %v", gotCode, tt.wantCode)
+			}
+		})
+	}
+}
+
+func Test_sampleUseCase_GetWeight_2(t *testing.T) {
+	type fields struct {
+		restAPI rest_api.RestAPI
+	}
+	type args struct {
+		in structs.GetWeight
+	}
+	tests := []struct {
+		name         string
+		fields       fields
+		args         args
+		wantOut      []response.WeightResponse
+		wantHttpCode int
+		wantCode     string
+		wantErr      bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			uc := &sampleUseCase{
+				restAPI: tt.fields.restAPI,
+			}
+			tt.args.in.Limit = 10
+			tt.args.in.Offset = 10
+			gotOut, gotHttpCode, gotCode, err := uc.GetWeight(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetWeight(2) error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotOut, tt.wantOut) {
+				t.Errorf("GetWeight(2) gotOut = %v, want %v", gotOut, tt.wantOut)
+			}
+			if gotHttpCode != tt.wantHttpCode {
+				t.Errorf("GetWeight(2) gotHttpCode = %v, want %v", gotHttpCode, tt.wantHttpCode)
+			}
+			if gotCode != tt.wantCode {
+				t.Errorf("GetWeight(2) gotCode = %v, want %v", gotCode, tt.wantCode)
+			}
+		})
+	}
+}
+
+func Test_sampleUseCase_GetWeight_3(t *testing.T) {
+	type fields struct {
+		restAPI rest_api.RestAPI
+	}
+	type args struct {
+		in structs.GetWeight
+	}
+	tests := []struct {
+		name         string
+		fields       fields
+		args         args
+		wantOut      []response.WeightResponse
+		wantHttpCode int
+		wantCode     string
+		wantErr      bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			uc := &sampleUseCase{
+				restAPI: tt.fields.restAPI,
+			}
+			tt.args.in.Limit = 10
+			tt.args.in.Offset = 20
+			gotOut, gotHttpCode, gotCode, err := uc.GetWeight(tt.args.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetWeight(3) error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotOut, tt.wantOut) {
+				t.Errorf("GetWeight(3) gotOut = %v, want %v", gotOut, tt.wantOut)
+			}
+			if gotHttpCode != tt.wantHttpCode {
+				t.Errorf("GetWeight(3) gotHttpCode = %v, want %v", gotHttpCode, tt.wantHttpCode)
+			}
+			if gotCode != tt.wantCode {
+				t.Errorf("GetWeight(3) gotCode = %v, want %v", gotCode, tt.wantCode)
 			}
 		})
 	}
@@ -60,7 +149,7 @@ func Test_sampleUseCase_GetWeightById(t *testing.T) {
 		name         string
 		fields       fields
 		args         args
-		wantOut      []response.WeightResponse
+		wantOut      response.WeightResponse
 		wantHttpCode int
 		wantCode     string
 		wantErr      bool
@@ -72,6 +161,7 @@ func Test_sampleUseCase_GetWeightById(t *testing.T) {
 			uc := &sampleUseCase{
 				restAPI: tt.fields.restAPI,
 			}
+			*tt.args.in.WeightRecordId = "1"
 			gotOut, gotHttpCode, gotCode, err := uc.GetWeightById(tt.args.in)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetWeightById() error = %v, wantErr %v", err, tt.wantErr)
@@ -113,6 +203,11 @@ func Test_sampleUseCase_AddWeight(t *testing.T) {
 			uc := &sampleUseCase{
 				restAPI: tt.fields.restAPI,
 			}
+			tt.args.in.Min = 50
+			tt.args.in.Max = 55
+			tt.args.in.Date = time.Now().Format("2006-01-02")
+			tt.args.in.UserId = "Test Case"
+			tt.args.in.Source = "Test Case"
 			gotOut, gotHttpCode, gotCode, err := uc.AddWeight(tt.args.in)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("AddWeight() error = %v, wantErr %v", err, tt.wantErr)
@@ -154,6 +249,11 @@ func Test_sampleUseCase_UpdateWeight(t *testing.T) {
 			uc := &sampleUseCase{
 				restAPI: tt.fields.restAPI,
 			}
+			tt.args.in.Min = 50
+			tt.args.in.Max = 65
+			tt.args.in.Date = time.Now().Format("2006-01-02")
+			tt.args.in.UserId = "Test Case"
+			tt.args.in.Source = "Test Case"
 			gotOut, gotHttpCode, gotCode, err := uc.UpdateWeight(tt.args.in)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UpdateWeight() error = %v, wantErr %v", err, tt.wantErr)
